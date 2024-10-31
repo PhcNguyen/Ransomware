9354e9dd3a9a2a24d601710409caf0f9ccdcd5a4
PhcNguyen <phcnguyenz@proton.me>
2024-10-31 22:25:52 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (C) PhcNguyen Developers
+// Distributed under the terms of the Modified BSD License.
+
+
+using Ransomware.Sources.Application;
+class Program
+{
+    static async Task Main()
+    {
+        var scanner = new FileExplorer(); // Tạo một đối tượng FileExplorer
+        var fileDetails = scanner.Scan(1024 * 1024, FileExplorer.SortOrder.Descending); // Quét thư mục gốc
+
+        Console.WriteLine($"Số lượng file cụ thể được lưu: {fileDetails.Count}");
+
+        foreach (var fileDetail in fileDetails)
+        {
+            Console.WriteLine($"{fileDetail.FilePath} - {fileDetail.FormatSize()}");
+            await Task.Delay(300);
+        }
+    }
+}
---
+++ Sources/Utils/FileDetail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ransomware.Sources.Utils;
+public class FileDetail
+{
+    public string FileName { get; set; } // Tên file
+    public string FilePath { get; set; } // Đường dẫn đầy đủ
+    public long Size { get; set; } // Kích thước file
+
+    public FileDetail(string filePath, long fileSize)
+    {
+        FileName = Path.GetFileName(filePath);
+        FilePath = filePath;
+        Size = fileSize;
+    }
+
+    public string FormatSize()
+    {
+        if (Size >= 1024 * 1024 * 1024) // GB
+            return $"{Size / (1024.0 * 1024 * 1024):F2} GB";
+
+        else if (Size >= 1024 * 1024) // MB
+            return $"{Size / (1024.0 * 1024):F2} MB";
+
+        else if (Size >= 1024) // KB
+            return $"{Size / 1024.0:F2} KB";
+        
+        else
+            return $"{Size} bytes"; // bytes
+    }
+}
